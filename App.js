de1b39d7975b987dc318457c2769ab0d178e81ca
rahul-s-1110 <saini1792@gmail.com>
2023-10-18 08:09:11 +0530

    project setup

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ListItem from './app/screens/listItem';
+// import
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ListItem />
     </View>
   );
 }
---
+++ app/screens/listItem.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ListItem = () => {
+  return (
+    <View>
+      <Text>listItem</Text>
+    </View>
+  )
+}
+
+export default ListItem
+
+const styles = StyleSheet.create({})
